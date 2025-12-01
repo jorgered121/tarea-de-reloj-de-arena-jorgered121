2f026a86483141f40923f52d8cb64cae53873783
jorgered121 <jorge.luis.sanchez.rojo@gmail.com>
2025-12-01 16:26:18 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -3,5 +3,11 @@
 def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
     
-    # TODO: implementar la lógica para generar el reloj de arena en ASCII
-    pass
+    # TODO: implementar la lógica para generar el reloj de arena en ASCII}
+    for i in range(n//2 +1):
+    print(" " * i + simbolo * (n - 2 * i))
+        for i in range(n//2 -1, -1, -1):
+            print(" " * i + simbolo * (n - 2 * i))
+                print()
+                print()
+    
